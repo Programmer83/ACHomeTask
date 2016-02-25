ce4e2d974f62a5c96c8d48969d82972e530d8c52
Dima Tolm <dimatolm@gmail.com>
2016-02-25 13:32:28 +0200

    commit

--- .idea/src/MAIN.java
+++ .idea/src/MAIN.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 /**
  * Created by Dima Tolm on 24/02/2016.
  */
 public class MAIN {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please input number: ");
+        byte num = sc.nextByte();
+
+
+
+
 
         System.out.println("MAIN");
     }
