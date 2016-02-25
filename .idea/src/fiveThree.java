d7c4389e3da52bcab7cbf331462a8ee7499b97d0
Dima Tolm <dimatolm@gmail.com>
2016-02-25 16:34:10 +0200

    fourSeven

--- .idea/src/fiveThree.java
+++ .idea/src/fiveThree.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 /**
  * Created by Dima Tolm on 25/02/2016.
  */
 public class fiveThree {
+
+    public static void main(String[] args) {
+
+        int[] myNum = {5,8,3,8};
+
+        for (int i = 0 ; i < myNum.length; i++){
+            if (myNum[i] == 8) {
+ //               int res = res + i;
+            }
+        }
+
+//        System.out.println(res);
+    }
 }
---
+++ .idea/src/fourSeven.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/**
+ * Created by Dima Tolm on 25/02/2016.
+ */
+public class fourSeven {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter second number");
+        int num2 = sc.nextInt();
+
+        if (num1 % num2 == 0) {
+            System.out.println();
+
+        }
+
+        System.out.println(res);
+    }
+}
