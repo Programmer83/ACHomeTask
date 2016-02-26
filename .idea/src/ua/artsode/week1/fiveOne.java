6c3806c3a2f6bd36bb691f350a794cf8b811af73
Dima Tolm <dimatolm@gmail.com>
2016-02-26 16:40:34 +0200

    fiveTwo

---
+++ .idea/src/ua/artsode/week1/fiveOne.java
@@ -0,0 +1,18 @@
+package ua.artsode.week1;
+
+import java.util.Scanner;
+/**
+ * Created by Dima Tolm on 26/02/2016.
+ */
+public class fiveOne {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please insert decimal number: ");
+        int dnum = sc.nextInt();
+
+
+
+    }
+}
