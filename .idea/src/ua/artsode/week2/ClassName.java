28187c36b7e3b69b1831ae24e019840d90a6126d
Dima Tolm <dimatolm@gmail.com>
2016-02-27 19:58:41 +0200

    new

---
+++ .idea/src/ua/artsode/week2/ClassName.java
@@ -0,0 +1,26 @@
+package ua.artsode.week2;
+
+/**
+ * Created by Dima Tolm on 27/02/2016.
+ */
+public class ClassName {
+    public static void main(String[] args) {
+
+        int el = 234;
+        double dl = 234.23;
+
+        String name = "oleg";
+
+        int[] mas = new int [10];
+        mas[0] =el;
+
+        String city = "Kiev";
+
+        name = city;
+
+        city = null;
+
+        String test;
+    }
+
+}
---
+++ .idea/src/ua/artsode/week2/intro.java
@@ -0,0 +1,24 @@
+package ua.artsode.week2;
+
+/**
+ * Created by Dima Tolm on 27/02/2016.
+ */
+public class intro {
+    public static void main(String[] args) {
+        String[] mas = new String[10];
+
+        mas[1] = "Petro";
+        mas[2] = "Ivan";
+
+        int index = 234 + 34;
+
+        String el_1 = mas[-1];
+        String test = mas [index];
+
+        int[] nums = {1,2,3,4,5};
+        nums = new int[23];
+
+    }
+
+
+}
